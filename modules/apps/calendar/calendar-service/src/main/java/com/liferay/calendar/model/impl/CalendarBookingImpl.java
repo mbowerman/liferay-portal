9babb01a0bdc7cab2e0d34143021f51532c5e0bf
Michael Bowerman <michael.bowerman@liferay.com>
2016-02-25 09:24:59 -0800

    LPS-62735 Events spawned from a recurring event no longer display in the recycle bin

--- modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/model/impl/CalendarBookingImpl.java
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/model/impl/CalendarBookingImpl.java
@@ -139,6 +139,15 @@ public class CalendarBookingImpl extends CalendarBookingBaseImpl {
 	}
 
 	@Override
+	public boolean isMasterRecurringBooking() {
+		if (getRecurringCalendarBookingId() == getCalendarBookingId()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
 	public boolean isRecurring() {
 		if (Validator.isNotNull(getMasterRecurrence())) {
 			return true;
