3b3cf7f9090e638d8b5c3a4f8aca8fb8c406f530
Michael Bowerman <michael.bowerman@liferay.com>
2017-02-15 13:08:35 -0800

    LPS-66047 Do not allow the deletion of a ResourceAction that is still in use

--- portal-impl/src/com/liferay/portal/service/impl/ResourceActionLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourceActionLocalServiceImpl.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.NoSuchResourceActionException;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.ResourceActionsException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.ResourceAction;
@@ -212,9 +213,20 @@ public class ResourceActionLocalServiceImpl
 	}
 
 	@Override
-	public ResourceAction deleteResourceAction(ResourceAction resourceAction) {
+	public ResourceAction deleteResourceAction(ResourceAction resourceAction)
+		throws PortalException {
+
 		final String name = resourceAction.getName();
 		final long bitwiseValue = resourceAction.getBitwiseValue();
+		String actionId = resourceAction.getActionId();
+
+		List<String> actionIds = ResourceActionsUtil.getResourceActions(name);
+
+		if (actionIds.contains(actionId)) {
+			throw new ResourceActionsException(
+				"Cannot delete resource action " + actionId + " for resource " +
+					name + " because it is currently in use");
+		}
 
 		ActionableDynamicQuery.AddCriteriaMethod addCriteriaMethod =
 			new ActionableDynamicQuery.AddCriteriaMethod() {
