5def1aebec774c1ec0522d28751b61599a803fde
Fortunato Maldonado <fortunato.maldonado@liferay.com>
2017-12-05 12:46:41 -0800

    LPS-76299 Added a class that checks for mismatched schemaVersions and creates error log

--- modules/apps/foundation/portal/portal-spring-extender/src/main/java/com/liferay/portal/spring/extender/internal/context/ModuleApplicationContextExtender.java
+++ modules/apps/foundation/portal/portal-spring-extender/src/main/java/com/liferay/portal/spring/extender/internal/context/ModuleApplicationContextExtender.java
@@ -152,14 +152,18 @@ public class ModuleApplicationContextExtender extends AbstractExtender {
 		@Override
 		public void destroy() throws Exception {
 			for (ServiceRegistration<UpgradeStep>
-					upgradeStepServiceRegistration :
-						_upgradeStepServiceRegistrations) {
+				upgradeStepServiceRegistration :
+				_upgradeStepServiceRegistrations) {
 
 				upgradeStepServiceRegistration.unregister();
 			}
 
 			if (_component != null) {
 				_dependencyManager.remove(_component);
+			}
+
+			if (_mismatchComponent != null) {
+				_dependencyManager.remove(_mismatchComponent);
 			}
 		}
 
@@ -188,6 +192,7 @@ public class ModuleApplicationContextExtender extends AbstractExtender {
 		@Override
 		public void start() throws Exception {
 			_component = _dependencyManager.createComponent();
+			_mismatchComponent = _dependencyManager.createComponent();
 
 			BundleContext bundleContext =
 				ModuleApplicationContextExtender.this.getBundleContext();
@@ -197,6 +202,9 @@ public class ModuleApplicationContextExtender extends AbstractExtender {
 			_component.setImplementation(
 				new ModuleApplicationContextRegistrator(
 					_bundle, bundle, _serviceConfigurator));
+
+			_mismatchComponent.setImplementation(
+				new MismatchReleaseRegistrator(_bundle));
 
 			ClassLoader classLoader = new BundleResolverClassLoader(
 				_bundle, bundle);
@@ -225,34 +233,44 @@ public class ModuleApplicationContextExtender extends AbstractExtender {
 			String requireSchemaVersion = headers.get(
 				"Liferay-Require-SchemaVersion");
 
-			if (Validator.isNull(requireSchemaVersion)) {
-				_generateReleaseInfo();
-			}
+			_generateReleaseInfo(requireSchemaVersion);
 
 			_dependencyManager.add(_component);
+			_dependencyManager.add(_mismatchComponent);
 
 			_upgradeStepServiceRegistrations = _processInitialUpgrade(
 				classLoader);
 		}
 
-		private void _generateReleaseInfo() {
+		private void _generateReleaseInfo(String requireSchemaVersion) {
 			ServiceDependency serviceDependency =
 				_dependencyManager.createServiceDependency();
 
 			serviceDependency.setRequired(true);
 
-			serviceDependency.setService(
-				Release.class,
-				StringBundler.concat(
-					"(&(release.bundle.symbolic.name=",
-					_bundle.getSymbolicName(), ")(release.schema.version=",
-					String.valueOf(_bundle.getVersion()), "))"));
+			if (Validator.isNull(requireSchemaVersion)) {
+				serviceDependency.setService(
+					Release.class,
+					StringBundler.concat(
+						"(&(release.bundle.symbolic.name=",
+						_bundle.getSymbolicName(), ")(release.schema.version=",
+						String.valueOf(_bundle.getVersion()), "))"));
 
-			_component.add(serviceDependency);
+				_component.add(serviceDependency);
+			}
+			else {
+				serviceDependency.setService(
+					Release.class,
+					StringBundler.concat(
+						"(release.bundle.symbolic.name=",
+						_bundle.getSymbolicName(), ")"));
+
+				_mismatchComponent.add(serviceDependency);
+			}
 		}
 
 		private List<ServiceRegistration<UpgradeStep>>
-			_processInitialUpgrade(ClassLoader classLoader) {
+		_processInitialUpgrade(ClassLoader classLoader) {
 
 			Dictionary<String, String> headers = _bundle.getHeaders();
 
@@ -277,7 +295,7 @@ public class ModuleApplicationContextExtender extends AbstractExtender {
 				if (_log.isDebugEnabled()) {
 					_log.debug(
 						"Unable to read service.properties for bundle " +
-							_bundle.getSymbolicName());
+						_bundle.getSymbolicName());
 				}
 			}
 
@@ -396,6 +414,7 @@ public class ModuleApplicationContextExtender extends AbstractExtender {
 		private final Bundle _bundle;
 		private org.apache.felix.dm.Component _component;
 		private final DependencyManager _dependencyManager;
+		private org.apache.felix.dm.Component _mismatchComponent;
 		private List<ServiceRegistration<UpgradeStep>>
 			_upgradeStepServiceRegistrations;
 
